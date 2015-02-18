e2806fef3390d011a07a1ecf4eed3f86d936ee1d
Steve Caldwell <steve@straphq.com>
2015-02-18 16:24:50 -0500

    attempting to get create script going from a template project

---
+++ bin/templates/framework/com.straphq.teststrapkit/com.straphq.teststrapkitTests/com_straphq_teststrapkitTests.swift
@@ -0,0 +1,36 @@
+//
+//  com_straphq_teststrapkitTests.swift
+//  com.straphq.teststrapkitTests
+//
+//  Created by scald on 2/18/15.
+//  Copyright (c) 2015 strap. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class com_straphq_teststrapkitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
